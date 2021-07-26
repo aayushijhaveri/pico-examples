8913d1acbe2c8a0edcd1a098f79138f0a0a1e031
Aayushi Jhaveri <aayushi.jhaveri@raspberrypi.com>
2021-07-26 16:52:39 +0100

    Added display code

--- gpio/hd44780/hd44780.c
+++ gpio/hd44780/hd44780.c
@@ -9,14 +9,12 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-#ifdef PICO_DEFAULT_LED_PIN
-#define LED_PIN PICO_DEFAULT_LED_PIN
-#endif
 
 //set pins
-const int rs = 0;
-const int rw = 1;
-const int d7 = 2;
+const int rs = 11;
+const int en = 14;
+const int rw = 10;
+const int d7 = 2 ;
 const int d6 = 3;
 const int d5 = 4;
 const int d4 = 5;
@@ -25,16 +23,71 @@ const int d2 = 7;
 const int d1 = 8;
 const int d0 = 9;
 
-void wait(){
-  sleep_ms(100);  
+
+void wait(int wait_time){
+  sleep_ms(wait_time);  
 }
 
+void toggle_enable(){
+    wait(1);
+    gpio_put(en,false);
+    wait(1);
+    gpio_put(en,true);
+    wait(100);
+
+}
+void write_byte_8bit(uint8_t message, bool rs_val){
+
+    gpio_put(d0, (message & 0x01) == 0x01);
+    gpio_put(d1, (message & 0x02) == 0x02);
+    gpio_put(d2, (message & 0x04) == 0x04);
+    gpio_put(d3, (message & 0x08) == 0x08);
+    gpio_put(d4, (message & 0x10) == 0x10);
+    gpio_put(d5, (message & 0x20) == 0x20);
+    gpio_put(d6, (message & 0x40) == 0x40);
+    gpio_put(d7, (message & 0x80) == 0x80);
+    gpio_put(rs, rs_val);
+
+    toggle_enable();
+
+}
+void hd44780_init(){
+    wait(300);
+
+    write_byte_8bit(0x30, false);
+
+    write_byte_8bit(0x30, false);
+
+    write_byte_8bit(0x30, false);
+
+    write_byte_8bit(0x30, false);
+    write_byte_8bit(0x08, false);
+
+    write_byte_8bit(0x01, false);
+
+    write_byte_8bit(0x06, false);
+
+
+    //display message
+    
+    printf("message below");
+    write_byte_8bit(0x48, true);
+    write_byte_8bit(0x45, true);
+    write_byte_8bit(0x4C, true);  
+    write_byte_8bit(0x4C, true);
+    write_byte_8bit(0x00, true);
+
+    wait(10000);
+
+}
 int main() {
     stdio_init_all();
+
     printf("Hello, 7segment - press button to count down!\n");
 
-    //initilaise pins 
+    //initialise pins 
     gpio_init(rs);
+    gpio_init(en);
     gpio_init(rw);
     gpio_init(d7);
     gpio_init(d6);
@@ -56,44 +109,13 @@ int main() {
     gpio_set_dir(d2, GPIO_OUT);
     gpio_set_dir(d1, GPIO_OUT);
     gpio_set_dir(d0, GPIO_OUT);
+    gpio_set_dir(en, GPIO_OUT);
+
 
     hd44780_init();
 }
 
-void write_byte_8bit(uint8_t message, bool rs_val){
-
-    gpio_put(d0, (message & 0x01) == 0x01);
-    gpio_put(d1, (message & 0x02) == 0x02);
-    gpio_put(d2, (message & 0x04) == 0x04);
-    gpio_put(d3, (message & 0x08) == 0x08);
-    gpio_put(d4, (message & 0x10) == 0x10);
-    gpio_put(d5, (message & 0x20) == 0x20);
-    gpio_put(d6, (message & 0x40) == 0x40);
-    gpio_put(d7, (message & 0x80) == 0x80);
-
-    wait();
-
-}
-
-void hd44780_init(){
-    write_byte_8bit(0x30, false);
-    write_byte_8bit(0x30, false);
-    write_byte_8bit(0x30, false);
-    write_byte_8bit(0x08, false);
-    write_byte_8bit(0x01, false);
-    write_byte_8bit(0x07, false);
-
-    //display message
-    
-    write_byte_8bit(0x48, true);
-    write_byte_8bit(0x45, true);
-    write_byte_8bit(0x4C, true);
-    write_byte_8bit(0x4C, true);
-    write_byte_8bit(0x4F, true);
-
-}
-
-void reset(){
+void reset(){ 
     
 }
 
